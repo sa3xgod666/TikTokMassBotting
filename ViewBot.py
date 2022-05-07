8c58f250ac3a4523cda9f6fb940be5ad17c5e18e
sa3xgod666 <103939909+sa3xgod666@users.noreply.github.com>
2022-05-07 13:29:49 +0300

    Update ViewBot.py

--- ViewBot.py
+++ ViewBot.py
@@ -103,7 +103,7 @@ def ClearURI(link):
 
 if (__name__ == "__main__"):
     Clear()
-    itemID       = "https://www.tiktok.com/@romancev768/video/7088972534418361601?is_copy_url=1&is_from_webapp=v1"
+    itemID       = "https://www.tiktok.com/@danya_milokhin/video/7094881708616486146?is_copy_url=1&is_from_webapp=v1"
     amount       = 0
     
     Proxytype    = "https"
